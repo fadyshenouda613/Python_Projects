accf3069e833a57383ff91c33dc80f75f7bec33e
Fady Shenouda <99507799+fadyshenouda613@users.noreply.github.com>
2022-05-05 03:36:37 -0400

    Create paddle.py

---
+++ pingpong/paddle.py
@@ -0,0 +1,21 @@
+import turtle
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.turtlesize(stretch_wid=5, stretch_len=1)
+        self.speed("fastest")
+        self.goto(position)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), y=new_y)
+
+    def go_down(self):
+        new_y = self.ycor() + -20
+        self.goto(self.xcor(), y=new_y)
